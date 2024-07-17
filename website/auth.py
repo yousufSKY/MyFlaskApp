25daf5a986d088ba064e339fe2144b6ee0504fef
yousuf <skyousuf22@outlook.com>
2024-07-17 23:03:40 +0530

    updates

--- website/auth.py
+++ website/auth.py
@@ -4,7 +4,7 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return render_template("login.html", text="testing the page")
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
