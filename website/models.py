a28fd644dcd97e20f2d0b3113357aa9f132f41f2
yousuf <skyousuf22@outlook.com>
2024-07-21 12:41:23 +0530

    updated all

---
+++ website/models.py
@@ -0,0 +1,24 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+    
+class PDF(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(120), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    first_name = db.Column(db.String(150), nullable=False)
+    notes = db.relationship('Note', backref='user', lazy=True)
+    pdfs = db.relationship('PDF', backref='user', lazy=True)
